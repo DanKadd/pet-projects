139620661eaba0d5e1c8467b2732c4ce88d947ae
d.horovenkov <d.horovenkov@ovkgrp.ru>
2022-11-11 22:10:45 +0300

    lasted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.scss';
 import { Success } from './components/Success';
 import { Users } from './components/Users';
@@ -6,10 +6,53 @@ import { Users } from './components/Users';
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [invaited, setInvaited] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const onClickInvited = (id) => {
+    if (invaited.includes(id)) {
+      setInvaited((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setInvaited((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSuccess = () => setSuccess(true);
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success count={invaited.length} />
+      ) : (
+        <Users
+          isLoading={isLoading}
+          items={users}
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          invaited={invaited}
+          onClickInvited={onClickInvited}
+          onClickSuccess={onClickSuccess}
+        />
+      )}
     </div>
   );
 }
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -2,27 +2,31 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ items, isLoading, searchValue, onChangeSearchValue, invaited, onClickInvited, onClickSuccess }) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input value={searchValue} onChange={onChangeSearchValue} type="text" placeholder="Найти пользователя..." />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
-          <Skeleton />
-          <Skeleton />
-          <Skeleton />
+          {[...Array(5)].map((_, index) => <Skeleton key={index} />)}
         </div>
       ) : (
         <ul className="users-list">
-          <User />
+          {items.filter((item) => {
+            const fullName = `${item.first_name} ${item.last_name}`;
+            return fullName.includes(searchValue) || item.email.includes(searchValue);
+          }).map((value) => <User key={value.id} isInvited={invaited.includes(value.id)} onClickInvited={onClickInvited} {...value} />)}
         </ul>
       )}
-      <button className="send-invite-btn">Отправить приглашение</button>
+      {
+        invaited.length >= 3 ? <button onClick={onClickSuccess} type='button' className="send-invite-btn">Отправить приглашение</button> : null 
+      }
+      
     </>
   );
 };
